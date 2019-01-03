0fffe531f445ac6dadcdd967fe7917a3bd943c69
Jens Wouters <jens_wouters@live.be>
2019-01-03 16:52:55 +0100

    corsmapping

--- src/main/java/be/ucll/da/dentravak/Application.java
+++ src/main/java/be/ucll/da/dentravak/Application.java
@@ -9,7 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.math.BigDecimal;
 
@@ -38,5 +41,15 @@ public class Application {
             repository.save(new Sandwich("Boulet", "Boulet en juunkes", new BigDecimal("4.50")));
 
         };
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
+
     }
 }
