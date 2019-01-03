0b1336fa65d5294e96ab37d665d1ac6e1dc6ee40
Jens Wouters <jens_wouters@live.be>
2019-01-03 13:22:23 +0100

    merge commit

--- src/main/java/be/ucll/da/dentravak/Application.java
+++ src/main/java/be/ucll/da/dentravak/Application.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.math.BigDecimal;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
--- src/main/java/be/ucll/da/dentravak/controller/SandwichController.java
+++ src/main/java/be/ucll/da/dentravak/controller/SandwichController.java
@@ -1,32 +1,56 @@
 package be.ucll.da.dentravak.controller;
 
 import be.ucll.da.dentravak.domain.Sandwich;
+import be.ucll.da.dentravak.model.SandwichPreferences;
 import be.ucll.da.dentravak.repository.SandwichRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import javax.naming.ServiceUnavailableException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Optional;
 import java.util.UUID;
+
+//import javax.inject.Inject;
 
 @RestController
 public class SandwichController {
 
+    //@Inject
     private SandwichRepository repo;
+
+    //@Inject
+    private RestTemplate restTemplate;
 
     public SandwichController(SandwichRepository repo ) {
         this.repo = repo;
     }
 
     @RequestMapping("/sandwiches")
-    public List<Sandwich> sandwich() {
-        /*return SandwichBuilder.aSandwich()
-                .withIngredients("Kaas & Hesp")
-                .withName("Smoske")
-                .withPrice(new BigDecimal("3.50"))
-                .build();*/
-
-        return (List) repo.findAll();
+    public Iterable<Sandwich> sandwiches() {
+        return repo.findAll();
+//        try {
+//            SandwichPreferences preferences = getPreferences("ronald.dehuysser@ucll.be");
+//            //TODO: sort allSandwiches by float in preferences
+//            Iterable<Sandwich> allSandwiches = repo.findAll();
+//            return allSandwiches;
+//        } catch (ServiceUnavailableException e) {
+//            return repo.findAll();
+//        }
     }
+
+//    @RequestMapping("/sandwiches")
+//    public List<Sandwich> sandwich() {
+//        /*return SandwichBuilder.aSandwich()
+//                .withIngredients("Kaas & Hesp")
+//                .withName("Smoske")
+//                .withPrice(new BigDecimal("3.50"))
+//                .build();*/
+//
+//        return (List) repo.findAll();
+//    }
     @RequestMapping(value = "/sandwiches/{id}", method = RequestMethod.GET)
     public Sandwich getSandwichById(@PathVariable(value="id") UUID id) {
         return repo.findById(id).get();
@@ -49,4 +73,30 @@ public class SandwichController {
         return repo.save(s.get());
 
     }
+
+    // why comment: for testing
+    @GetMapping("/getpreferences/{emailAddress}")
+    public SandwichPreferences getPreferences(@PathVariable String emailAddress) throws RestClientException, ServiceUnavailableException {
+        URI service = recommendationServiceUrl()
+                .map(s -> s.resolve("/recommend/" + emailAddress))
+                .orElseThrow(ServiceUnavailableException::new);
+        return restTemplate
+                .getForEntity(service, SandwichPreferences.class)
+                .getBody();
+    }
+
+    public Optional<URI> recommendationServiceUrl() {
+        try {
+            return Optional.of(new URI("http://localhost:8081"));
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    public Optional<URI> recommendationServiceUrl() {
+//        return discoveryClient.getInstances("recommendation")
+//                .stream()
+//                .map(si -> si.getUri())
+//                .findFirst();
+//    }
 }
