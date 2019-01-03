3fc7fd65b60dc8a601141e915ebabdfa68e7ed4c
arnaudancis <arnaud.ancis@student.ucll.be>
2019-01-03 14:47:06 +0100

    ports

--- src/main/resources/public/js/orders.js
+++ src/main/resources/public/js/orders.js
@@ -5,13 +5,7 @@ function getToday(){
     var mm = today.getMonth()+1; //January is 0!
     var yyyy = today.getFullYear();
 
-    if(dd<10) {
-        dd = '0'+dd
-    }
 
-    if(mm<10) {
-        mm = '0'+mm
-    }
 
     today = dd + '-' + mm + '-' + yyyy;
     var h1 = document.getElementById('date');
