7986c95e3ec87d985e06b5cd206bd0d945fa237c
ArnaudAncis <arnaud.ancis@student.ucll.be>
2019-01-03 19:31:21 +0100

    inject bij healthcontroller

--- src/main/java/be/ucll/da/dentravak/controller/HealthController.java
+++ src/main/java/be/ucll/da/dentravak/controller/HealthController.java
@@ -1,12 +1,18 @@
 package be.ucll.da.dentravak.controller;
 
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
+
 @RestController
 public class HealthController {
+
+    @Inject
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/health-check")
     public ResponseEntity<String> myCustomCheck() {
